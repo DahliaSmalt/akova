fbccdcb052cfcc7e584e3ccffa796e383461ef82
dahliasmalt <mail@dsma.lt>
2015-02-04 18:20:48 +0000

    ok

--- server.js
+++ server.js
@@ -9,15 +9,9 @@ var path = require('path');
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
-var db = require('mongoose');
+var db = require('mongoose')
 db.connect(process.env.MONGOLAB_URI);
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
